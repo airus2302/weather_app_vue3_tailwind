ae83407a247d1c72ab92ae979125075b5ea87429
airus2302 <airus2302@gmail.com>
2022-08-21 22:13:29 +0900

    lesson-9 City View Layouta

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,20 @@
 <template>
   <main class="container text-white">
     <div class="pt-4 mb-8 relative">
-      <input 
-        type="text" 
-        v-model="searchQuery"
-        @input="getSearchResults"
-        placeholder="Search for a city or state" 
-        class="py-2 px-1 w-full bg-transparent border-b 
+      <input type="text" v-model="searchQuery" @input="getSearchResults" placeholder="Search for a city or state" class="py-2 px-1 w-full bg-transparent border-b 
         focus:border-weather-secondary focus:outline-none 
-        focus:shadow-[0px_1px_0_0_#004E71]" 
-      />
-      <ul
-        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
-        v-if="mapboxSearchResults"
-      >
+        focus:shadow-[0px_1px_0_0_#004E71]" />
+      <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults">
         <p class="py-2" v-if="searchError">
           Sorry, something went wrong, please try again.
         </p>
-        <p
-          class="py-2"
-          v-if="!searchError && mapboxSearchResults.length === 0"
-        >
+        <p class="py-2" v-if="!searchError && mapboxSearchResults.length === 0">
           No results match your query, try a different term.
         </p>
         <template v-else>
-          <li
-            v-for="searchResult in mapboxSearchResults"
-            :key="searchResult.id"
-            class="py-2 cursor-pointer"
-            @click="previewCity(searchResult)"
-          >
+          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)">
             {{ searchResult.place_name }}
           </li>
         </template>
@@ -80,7 +65,7 @@ const getSearchResults = () => {
       } catch {
         searchError.value = true
       }
-      
+
       return;
     }
     mapboxSearchResults.value = null;
