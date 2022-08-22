789002809fc5ecdb1ad82c3dd5a8179d152853bb
airus2302 <airus2302@gmail.com>
2022-08-22 20:16:42 +0900

    lesson-10 Adding Cities to Local Storage

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -8,8 +8,10 @@
                 </div>
             </RouterLink>
             <div class="flex gap-3 flex-1 justify-end">
-                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer" @click="toggleModal"></i>
-                <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+                    @click="toggleModal"></i>
+                <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+                    @click="addCity" v-if="route.query.preview"></i>
             </div>
             <BaseModal :modal-active="modalActive" @close-modal="toggleModal">
                 <div class="text-black">
@@ -21,17 +23,17 @@
                     <h2 class="text-2xl">How it works:</h2>
                     <ol class="list-decimal list-inside mb-4">
                         <li>
-                        Search for your city by entering the name into the
-                        search bar.
+                            Search for your city by entering the name into the
+                            search bar.
                         </li>
                         <li>
-                        Select a city within the results, this will take
-                        you to the current weather for your selection.
+                            Select a city within the results, this will take
+                            you to the current weather for your selection.
                         </li>
                         <li>
-                        Track the city by clicking on the "+" icon in the
-                        top right. This will save the city to view at a
-                        later time on the home page.
+                            Track the city by clicking on the "+" icon in the
+                            top right. This will save the city to view at a
+                            later time on the home page.
                         </li>
                     </ol>
 
@@ -41,16 +43,43 @@
                         the city within the home page. At the bottom of the
                         page, there will be am option to delete the city.
                     </p>
-                    </div>
+                </div>
             </BaseModal>
-        </nav>    
+        </nav>
     </header>
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router';
+import { RouterLink, useRoute, useRouter } from 'vue-router';
+import { uid } from 'uid';
 import { ref } from 'vue';
 import BaseModal from './BaseModal.vue';
+
+const savedCities = ref([]);
+const route = useRoute();
+const router = useRouter();
+const addCity = () => {
+    if (localStorage.getItem('savedCities')) {
+        savedCities.value = JSON.parse(localStorage.getItem('savedCities'));
+    }
+
+    const locationObj = {
+        id: uid(),
+        state: route.params.state,
+        city: route.params.city,
+        coords: {
+            lat: route.query.lat,
+            lng: route.query.lng,
+        },
+    };
+
+    savedCities.value.push(locationObj);
+    localStorage.setItem('savedCities', JSON.stringify(savedCities.value));
+
+    let query = Object.assign({}, route.query);
+    delete query.preview;
+    router.replace({ query });
+};
 
 const modalActive = ref(null);
 const toggleModal = () => {
